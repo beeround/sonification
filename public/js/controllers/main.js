6932b808f31e818683626786de2a4abc9149ad79
Karsten Schütz <karstenschuetz@web.de>
2017-05-05 14:17:41 +0200

    Added network chart

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -1,15 +1,22 @@
 'use strict';
-angular.module('sonificationAPP.controllers.main', []).
+angular.module('sonificationAPP.controllers.main', [])
 
-controller('mainCtrl', function ($scope, $http, $location) {
-    $scope.goToCompare = function () {
-        $location.path('/app/compare');
-    }
+    .controller('mainCtrl', function ($scope, $http, $location) {
+        $scope.goToCompare = function () {
+            $location.path('/app/compare');
+        }
+    })
 
-}).
-    controller('chartCtrl', function ($scope) {
-    $scope.test = "Chart";
+    .controller('chartCtrl', function ($scope) {
+        $scope.chartTitle = 'Test';
 
+        getChart();
 
-});
+        function getChart() {
+            var ctx = document.getElementById("myChart");
+            var myChart = new Chart(ctx, {
+
+            });
+        }
+    });
 
